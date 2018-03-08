35a51a0c3f483d24da6fabcc64af882cd7344c09
Youxi Li <yol107@ucsd.edu>
2018-03-07 22:12:59 -0800

    fixed bug

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,18 +8,21 @@ $(document).ready(function() {
 // Function that is called when the document is ready.
 function initializePage() {
     $.get('/lab/today', onServerResponse);
+    console.log(labElementId);
 }
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
     var labElementId = getLabElementId(data.id);
+    console.log(labElementId);
     var labElement = $(labElementId);
+    console.log(labElementId);
     labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
